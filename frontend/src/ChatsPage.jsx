61e4029fb40e3d95391c9de1c424da800d92cb1e
pemex69 <randomtrtrandom@gmail.com>
2023-11-24 07:42:32 -0600

    front fix

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,9 +1,8 @@
-import { PrettyChatWindow } from 'react-chat-engine-pretty';
 import PropTypes from 'prop-types';
-
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
-    console.log("\nPROPS: " + props);
+    console.log("\nPROPS: " + props + "\n");
     return (
         <div style={{
             height: '100vh',
@@ -12,7 +11,7 @@ const ChatsPage = (props) => {
                 projectId="5364cd84-7ba2-410d-b436-44bdac5c1ca4"
                 username={props.user.username}
                 secret={props.user.secret}
-                style={{ height: '100%' }}
+                style={{ height: '100vh' }}
             />
         </div>
     )
@@ -26,32 +25,3 @@ ChatsPage.propTypes = {
 };
 
 export default ChatsPage;
-
-
-
-
-
-// import PropTypes from 'prop-types';
-// import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
-
-// const ChatsPage = (props) => {
-//     const chatProps = useMultiChatLogic(
-//         '5364cd84-7ba2-410d-b436-44bdac5c1ca4',
-//         props.user.username,
-//         props.user.secret
-//     );
-//     return (
-//         <div style={{ height: '100vh' }}>
-//             <MultiChatSocket {...chatProps} />
-//             <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
-//         </div>
-
-//     )
-// }
-// ChatsPage.propTypes = {
-//     user: PropTypes.shape({
-//         username: PropTypes.string.isRequired,
-//         secret: PropTypes.string.isRequired,
-//     }).isRequired,
-// };
-// export default ChatsPage;
