6359d6f19c0101e08281c89d56c312fdb88341c3
pemex69 <randomtrtrandom@gmail.com>
2023-11-24 01:22:44 -0600

    react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+const { createRoot } = ReactDOM;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <App />
-
-)
+);
